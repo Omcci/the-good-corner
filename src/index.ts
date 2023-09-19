6716516e0d12ad6e1cc749162fe15c815a763bf8
Omci <melloulchi.omar@gmail.com>
2023-09-19 16:37:29 +0200

    GET and POST routes created

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,21 @@
-import express from "express"
+import express, { response } from "express"
+import { ads } from "./ads"
 
 const server = express()
 
 server.get("/", (req, res) => {
     res.send("Hello from Express server.")
 })
+
+// GET /ads
+server.get('/ads', (req,res) => {
+    res.json({ads})
+})
+// POST /ads
+server.post('/ads', (req,res) => {
+    res.send("Post is working")
+})
+
 const PORT = 4000
 server.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`)
