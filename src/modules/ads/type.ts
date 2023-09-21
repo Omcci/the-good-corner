a8da4881720311433b755178d9df0316da0a3bbc
Omci <melloulchi.omar@gmail.com>
2023-09-21 22:37:07 +0200

    Added type for Ad

---
+++ src/modules/ads/type.ts
@@ -0,0 +1,11 @@
+export type Ad = {
+    id: number;
+    title: string;
+    description: string;
+    owner: string;
+    price: number;
+    picture: string;
+    createdAt: string;
+    location: string;
+    category_id: number;
+}
