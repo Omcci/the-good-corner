e20cb411071809bab1a35d15f41fa1a54b48c6d3
Omci <melloulchi.omar@gmail.com>
2023-09-22 20:18:26 +0200

    Added default category creation automatically at the initialization of the server

--- src/config/server.ts
+++ src/config/server.ts
@@ -3,16 +3,19 @@ import cors from "cors";
 const app = express();
 import { router as moduleRouter } from "../modules/index";
 import { dataSource } from "./database";
+import Category from "../entities/category";
 
 app.use(express.json());
 app.use(cors());
-app.use('/api', moduleRouter);
+app.use("/api", moduleRouter);
 
-const PORT = 4000
+const PORT = 4000;
 app.listen(PORT, async () => {
-    await dataSource.initialize();
-    console.log(`Server listening on port ${PORT}`);
-  });
-  
+  await dataSource.initialize();
+  await Category.saveNewCategoryIfNotExisting({ name: "Automobile" });
+  await Category.saveNewCategoryIfNotExisting({ name: "Habillement" });
+  await Category.saveNewCategoryIfNotExisting({ name: "Autre" });
+  console.log(`Server listening on port ${PORT}`);
+});
 
 export { app };
