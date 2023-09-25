b211666d63174475793147e38750c961e9492015
Omci <melloulchi.omar@gmail.com>
2023-09-25 23:48:08 +0200

    removed tag from being automatically created at init

--- src/config/server.ts
+++ src/config/server.ts
@@ -6,20 +6,23 @@ import { dataSource } from "./database";
 import Category from "../entities/category";
 import Tag from "../entities/tag";
 
+
 app.use(express.json());
-app.use(cors());
-app.use("/api", moduleRouter);
+// app.use(express.json());
+// app.use(cors());
+// app.use("/api", moduleRouter);
 
 const PORT = 4000;
 app.listen(PORT, async () => {
-  await dataSource.initialize();
-  await Category.saveNewCategoryIfNotExisting({ name: "Automobile" });
-  await Category.saveNewCategoryIfNotExisting({ name: "Habillement" });
-  await Category.saveNewCategoryIfNotExisting({ name: "Autre" });
-  await Tag.saveNewTagIfNotExisting({ name: "Auto" });
-  await Tag.saveNewTagIfNotExisting({ name: "Fashion" });
-  await Tag.saveNewTagIfNotExisting({ name: "Autre" });
-  console.log(`Server listening on port ${PORT}`);
-});
-
+    try {
+      await dataSource.initialize();
+      await Category.saveNewCategoryIfNotExisting({ name: "Automobile" });
+      await Category.saveNewCategoryIfNotExisting({ name: "Habillement" });
+      await Category.saveNewCategoryIfNotExisting({ name: "Autre" });
+  
+      console.log(`Server listening on port ${PORT}`);
+    } catch (error) {
+      console.error("An error occurred:", error);
+    }
+  });
 export { app };
