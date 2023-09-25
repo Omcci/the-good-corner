978f3e23d75f4e26ec18afd2ce6bea3ec5c462b1
Omci <melloulchi.omar@gmail.com>
2023-09-25 23:47:18 +0200

    added entities Category and Tag in dataSource

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,11 +1,13 @@
 import { DataSource } from "typeorm";
 import Ad from "../entities/ad";
+import Category from "../entities/category";
+import Tag from "../entities/tag";
 
 
 const dataSource = new DataSource({
     type: "sqlite",
     database: "db.sqlite",
-    entities: [Ad],
+    entities: [Ad, Category, Tag],
     synchronize: true,
 });
 
