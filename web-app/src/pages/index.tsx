86351b73cee9adc5f47f3f1980d72e88e4706054
Omci <melloulchi.omar@gmail.com>
2023-09-27 22:16:08 +0200

    Created a Layout component

--- web-app/src/pages/index.tsx
+++ web-app/src/pages/index.tsx
@@ -2,6 +2,8 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
+import styled from "styled-components";
+
 
 const inter = Inter({ subsets: ['latin'] })
 
