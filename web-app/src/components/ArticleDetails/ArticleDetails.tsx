af439ff309dc3ebcfbbe94c6d84e1a4d7085fb24
Omci <melloulchi.omar@gmail.com>
2023-09-27 22:39:24 +0200

    Created ArticleDetails component to show details of an ad

---
+++ web-app/src/components/ArticleDetails/ArticleDetails.tsx
@@ -0,0 +1,35 @@
+// import { ButtonLikeLink } from "../Link/ButtonLikeLink";
+// import { MailIcon } from "../Icons/MailIcon";
+
+import * as styled from "./ArticleDetails.styled";
+// import { Separator } from "../Separator/Separator";
+
+export default function Container() {
+  return (
+    <>
+      <h2>Table</h2>
+      <styled.Container>
+        <styled.ImageContainer>
+          <styled.Image src={`/images/1.webp`} />
+        </styled.ImageContainer>
+        <styled.Info>
+          <styled.Price>120 €</styled.Price>
+          <div>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Non, iusto!
+            Voluptates repudiandae asperiores quia. Blanditiis repellat minima
+            adipisci, aliquam nulla unde quam architecto eligendi, voluptatum,
+            perspiciatis laudantium sed eos voluptates?
+          </div>
+          {/* <Separator /> */}
+          <div>
+            Annoncée publiée par <b>Serge</b> aujourd'hui (9:32).
+          </div>
+          {/* <ButtonLikeLink href="mailto:serge@serge.com">
+            <MailIcon />
+            Envoyer un email
+          </ButtonLikeLink> */}
+        </styled.Info>
+      </styled.Container>
+    </>
+  );
+}
