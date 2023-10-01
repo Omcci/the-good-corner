94f44acb23167923d838d95758f6c4f1edb9eada
Omci <melloulchi.omar@gmail.com>
2023-10-01 18:17:39 +0200

    Modified Category page to let a user browse all ads from a specific category

--- web-app/src/pages/categories/[id].tsx
+++ web-app/src/pages/categories/[id].tsx
@@ -1,8 +1,52 @@
+import ArticleCard from "@/components/ArticleCard/ArticleCard";
+import { CardGrid } from "@/components/CardGrid/CardGrid";
+import { CATEGORIES } from "@/components/Layout/Layout";
+import Loader from "@/components/Loader/Loader";
+import { Article } from "@/types";
 import { useParams } from "next/navigation";
+import { use, useEffect, useState } from "react";
 
 export default function Category() {
   const params = useParams();
-  const id = params?.id;
+  const id = params?.id as string
 
-  return id ? `Catégorie ${id}` : "Loading…";
+  const [articles, setArticles] = useState<Article[] | null>(null)
+
+  useEffect(() => {
+    const fetchAds = async (categoryId: string) => {
+      const response = await fetch(`/api/ads?category=${categoryId}`)
+      const { ads } = await response.json() as { ads: Article[] }
+      setArticles(ads)
+    }
+
+    if (id) {
+      fetchAds(id)
+    }
+  }, [id])
+
+  const category = CATEGORIES.find((category) => category.id === +(id))
+  if (!category) {
+    return "La catégorie sélectionnée n'existe pas."
+  }
+  
+
+  return (
+    <>
+    <h1>{category.name}</h1>
+    <CardGrid>
+    {articles ? (
+      articles.map((article) => (
+        <ArticleCard
+          key={article.id}
+          id={article.id}
+          title={article.title}
+          price={article.price}
+          /> 
+      ))
+    ) : (
+      <Loader global />
+    )}
+    </CardGrid>
+    </>
+  )
 }
