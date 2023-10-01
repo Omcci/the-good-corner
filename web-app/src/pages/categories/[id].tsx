dccf157eff78c9c4dd03dc47c6b5ea578a5ff513
Omci <melloulchi.omar@gmail.com>
2023-10-01 18:19:12 +0200

    Created PageContainer component

--- web-app/src/pages/categories/[id].tsx
+++ web-app/src/pages/categories/[id].tsx
@@ -2,51 +2,51 @@ import ArticleCard from "@/components/ArticleCard/ArticleCard";
 import { CardGrid } from "@/components/CardGrid/CardGrid";
 import { CATEGORIES } from "@/components/Layout/Layout";
 import Loader from "@/components/Loader/Loader";
+import { PageContainer } from "@/components/PageContainer/PageContainer";
 import { Article } from "@/types";
 import { useParams } from "next/navigation";
 import { use, useEffect, useState } from "react";
 
 export default function Category() {
   const params = useParams();
-  const id = params?.id as string
+  const id = params?.id as string;
 
-  const [articles, setArticles] = useState<Article[] | null>(null)
+  const [articles, setArticles] = useState<Article[] | null>(null);
 
   useEffect(() => {
     const fetchAds = async (categoryId: string) => {
-      const response = await fetch(`/api/ads?category=${categoryId}`)
-      const { ads } = await response.json() as { ads: Article[] }
-      setArticles(ads)
-    }
+      const response = await fetch(`/api/ads?category=${categoryId}`);
+      const { ads } = (await response.json()) as { ads: Article[] };
+      setArticles(ads);
+    };
 
     if (id) {
-      fetchAds(id)
+      fetchAds(id);
     }
-  }, [id])
+  }, [id]);
 
-  const category = CATEGORIES.find((category) => category.id === +(id))
+  const category = CATEGORIES.find((category) => category.id === +id);
   if (!category) {
-    return "La catégorie sélectionnée n'existe pas."
+    return "La catégorie sélectionnée n'existe pas.";
   }
-  
 
   return (
-    <>
-    <h1>{category.name}</h1>
-    <CardGrid>
-    {articles ? (
-      articles.map((article) => (
-        <ArticleCard
-          key={article.id}
-          id={article.id}
-          title={article.title}
-          price={article.price}
-          /> 
-      ))
-    ) : (
-      <Loader global />
-    )}
-    </CardGrid>
-    </>
-  )
+    <PageContainer>
+      <h1>{category.name}</h1>
+      <CardGrid>
+        {articles ? (
+          articles.map((article) => (
+            <ArticleCard
+              key={article.id}
+              id={article.id}
+              title={article.title}
+              price={article.price}
+            />
+          ))
+        ) : (
+          <Loader global />
+        )}
+      </CardGrid>
+    </PageContainer>
+  );
 }
